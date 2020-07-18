7b4e7f0cd4e1945ed590e83336ea69c28b9a478a
Alex <eieghtbitdolphin@gmail.com>
2020-07-17 14:08:07 -1000

    linear regression is complete(mostly), will add a page later

--- magic.js
+++ magic.js
@@ -26,6 +26,7 @@ Papa.parse(file, {
         //initalValues.theta = gradientDescent(initalValues.theta,initalValues.X,initalValues.y,1,10);
         initalValues.theta = normalEqt(initalValues.X, initalValues.y);
         console.log(`Theta Values are now ${initalValues.theta}`);
+        console.log(`For a house built in 1983, the price is ${math.multiply(math.matrix([[1, 1940]]),initalValues.theta)}`);
     }
 })
 
@@ -61,13 +62,16 @@ const gradientDescent = (theta, X, y, alpha,iterations) => {
     let m = y.size()[0];
     let optimizedTheta = theta;
     for(var i=0; i < iterations; ++i) {
+        // the predictions with theta and how bad the error was (without power, since derived)
         let predictions = math.multiply(X, optimizedTheta);
         let derivedError = math.subtract(predictions,y);
-        let firstThetaRow = math.multiply((1/m),math.sum(math.dotMultiply(derivedError,math.column(X,0))));
-        let secondThetaRow = math.multiply((1/m),math.sum(math.dotMultiply(derivedError,math.column(X,1))));
-        optimizedTheta = math.subtract(optimizedTheta, math.multiply(alpha, math.matrix([[firstThetaRow],[secondThetaRow]])));
-        //console.log(`X's first column is ${math.column(X,1)}`)
-        //console.log(`first theta is ${firstThetaRow}, second theta is ${secondThetaRow}`)
+
+        let firstThetaRow = math.multiply((1/m),math.sum(math.dotMultiply(derivedError,math.column(X,0)))); // calculates the first feature row for X
+        let secondThetaRow = math.multiply((1/m),math.sum(math.dotMultiply(derivedError,math.column(X,1)))); // 2nd feature row for X
+        
+        // returns theta after every iteration
+        optimizedTheta = math.subtract(optimizedTheta, math.multiply(alpha, math.matrix([[firstThetaRow],[secondThetaRow]]))); 
+        
         //console.log(`${i+1} iteration | cost = ${computeCost(optimizedTheta,X,y)}`);
     }
     return optimizedTheta;
@@ -75,11 +79,13 @@ const gradientDescent = (theta, X, y, alpha,iterations) => {
 
 // Function that normalizes the data when put through
 const normalizeData = X => {
+    // normalizes the data for X, for quicker calculations
     let mean = math.mean(math.column(X,0));
     let std = math.std(math.column(X,0));
     return math.dotDivide(math.subtract(X,mean),std);
 }
 
+// Another method in reaching global minimum
 const normalEqt = (X,y) => {
     return math.multiply(math.inv(math.multiply(math.transpose(X),X)), math.multiply(math.transpose(X),y));
 }
